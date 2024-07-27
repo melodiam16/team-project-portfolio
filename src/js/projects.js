9f14382337571070cc46fc7c29fc753a0f926b39
Michael Janzer <michaeljanzer1996@gmail.com>
2024-07-27 13:50:11 +0300

    1

---
+++ src/js/projects.js
@@ -0,0 +1,31 @@
+let swiper = new Swiper('.swiper-container', {
+            slidesPerView: 'auto',
+            spaceBetween: 10,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            keyboard: {
+                enabled: true,
+                onlyInViewport: false,
+            },
+            mousewheel: true,
+            on: {
+                slideChange: function () {
+                    let nextButton = document.querySelector('.swiper-button-next');
+                    let prevButton = document.querySelector('.swiper-button-prev');
+                    nextButton.classList.toggle('swiper-button-disabled', !swiper.isEnd);
+                    prevButton.classList.toggle('swiper-button-disabled', !swiper.isBeginning);
+                },
+                init: function () {
+                    let nextButton = document.querySelector('.swiper-button-next');
+                    let prevButton = document.querySelector('.swiper-button-prev');
+                    nextButton.classList.toggle('swiper-button-disabled', !swiper.isEnd);
+                    prevButton.classList.toggle('swiper-button-disabled', !swiper.isBeginning);
+                }
+            }
+        });
