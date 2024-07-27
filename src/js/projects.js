ae9e83769f76649c14dddadd817faf09c8981745
Michael Janzer <michaeljanzer1996@gmail.com>
2024-07-27 17:44:04 +0300

    5

--- src/js/projects.js
+++ src/js/projects.js
@@ -16,16 +16,16 @@ let swiper = new Swiper('.swiper-container', {
             mousewheel: true,
             on: {
                 slideChange: function () {
-                    // Оновлюємо статус кнопок навігації
-                    var nextButton = document.querySelector('.swiper-button-next');
-                    var prevButton = document.querySelector('.swiper-button-prev');
+         
+                    let nextButton = document.querySelector('.swiper-button-next');
+                    let prevButton = document.querySelector('.swiper-button-prev');
                     nextButton.classList.toggle('swiper-button-disabled', !swiper.isEnd);
                     prevButton.classList.toggle('swiper-button-disabled', !swiper.isBeginning);
                 },
                 init: function () {
-                    // Оновлюємо статус кнопок при ініціалізації
-                    var nextButton = document.querySelector('.swiper-button-next');
-                    var prevButton = document.querySelector('.swiper-button-prev');
+                    
+                    let nextButton = document.querySelector('.swiper-button-next');
+                    let prevButton = document.querySelector('.swiper-button-prev');
                     nextButton.classList.toggle('swiper-button-disabled', !swiper.isEnd);
                     prevButton.classList.toggle('swiper-button-disabled', !swiper.isBeginning);
                 }
