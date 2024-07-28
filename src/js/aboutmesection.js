53c22a987d16e989cc5e607795fa9d87fe3875ca
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-07-29 00:21:55 +0300

    finnalle-reqwest-section-about-me/updeita/css

--- src/js/aboutmesection.js
+++ src/js/aboutmesection.js
@@ -1,28 +1,46 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-// import Swiper JS
+
 import Swiper from 'swiper';
-// import Swiper styles
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 import 'swiper/css';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css/navigation';
 
 // створив новий екземпляр accordion цієї біліотеки-класу
 const accordion = new Accordion('.accordion-container-first', {
   duration: 600,
   showMultiple: true,
+  function () {document.querySelector('.accordion-container-first').classList.add('opened')}
 });
-// створив новий екземпляр swiper цієї біліотеки-класу
 
-const swiper = new Swiper('.mySwiper', {
-  modules: [Navigation],
-  speed: 500,
+
+// створив новий екземпляр Swiper цієї біліотеки-класу
+
+new Swiper('.about-swiper', {
+  slidesPerView: 2,
+  spaceBetween: 1,
+  speed: 1000,
+  allowSlidePrev: false,
+  loop: true,
+  modules: [Navigation, Keyboard, Mousewheel],
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.button-next',
+  },
+  keyboard: {
+    enabled: false,
+    onlyInViewport: false,
+  },
+  mousewheel: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      width: 600,
+      loop: true,
+    },
+    1440: {
+      slidesPerView: 6,
+      width: 1200,
+      loop: false,
+    },
   },
 });
 
-// експорт моїх екземплярів бібліотек до кореневої папки main.js
-export { accordion };
-export { swiper };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { accordion } from './js/aboutmesection';
-import { swiper } from './js/aboutmesection';
+// import { accordion } from './js/aboutmesection';
+// import { swiper } from './js/aboutmesection';
+import './js/aboutmesection'
 import { Accordion as FaqAccordion } from './js/faq';
+
 // Реалізація гортання карток за допомогою Swiper
 import './js/reviews';
