145c6ae66920f2d8270c45af290de2eb511eb4b7
Michael Janzer <michaeljanzer1996@gmail.com>
2024-07-28 22:25:08 +0300

    new-start

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,31 +1,52 @@
-let swiper = new Swiper('.swiper-container', {
-            slidesPerView: 'auto',
-            spaceBetween: 10,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            keyboard: {
-                enabled: true,
-                onlyInViewport: false,
-            },
-            mousewheel: true,
-            on: {
-                slideChange: function () {
-                    let nextButton = document.querySelector('.swiper-button-next');
-                    let prevButton = document.querySelector('.swiper-button-prev');
-                    nextButton.classList.toggle('swiper-button-disabled', !swiper.isEnd);
-                    prevButton.classList.toggle('swiper-button-disabled', !swiper.isBeginning);
-                },
-                init: function () {
-                    let nextButton = document.querySelector('.swiper-button-next');
-                    let prevButton = document.querySelector('.swiper-button-prev');
-                    nextButton.classList.toggle('swiper-button-disabled', !swiper.isEnd);
-                    prevButton.classList.toggle('swiper-button-disabled', !swiper.isBeginning);
-                }
-            }
-        });
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const btnNextEl = document.querySelector('.swiper-button-next ');
+btnNextEl.style.color = '#FAFAFA';
+btnNextEl.style.position = 'static';
+btnNextEl.style.width = '64px';
+btnNextEl.style.height = '64px';
+btnNextEl.style.margin = '0';
+const btnPrevEl = document.querySelector('.swiper-button-prev ');
+btnPrevEl.style.position = 'static';
+btnPrevEl.style.width = '64px';
+btnPrevEl.style.height = '64px';
+btnPrevEl.style.color = '#FAFAFA';
+btnPrevEl.style.margin = '0';
+
+function upSwiperSlideDisplay() {
+  const swiperSlides = document.querySelectorAll('.swiper-slide');
+  if (window.innerWidth > 1440) {
+    swiperSlides.forEach(slide => {
+      slide.style.display = 'flex';
+    });
+  } else {
+    swiperSlides.forEach(slide => {
+      slide.style.display = 'block';
+    });
+  }
+}
+upSwiperSlideDisplay();
+window.addEventListener('resize', upSwiperSlideDisplay);
+
+export const mySwiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: false,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    4400: {
+      slidesPerView: 3,
+    },
+  },
+});
