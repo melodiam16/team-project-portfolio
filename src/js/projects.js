284a73fb17c5a5a631e6a9e381790643571ca3bd
Michael Janzer <michaeljanzer1996@gmail.com>
2024-07-28 12:41:56 +0300

    6

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,35 +1,18 @@
-let swiper = new Swiper('.swiper-container', {
-            slidesPerView: 'auto',
-            spaceBetween: 10,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            keyboard: {
-                enabled: true,
-                onlyInViewport: false,
-            },
-            mousewheel: true,
-            on: {
-                slideChange: function () {
-         
-                    let nextButton = document.querySelector('.swiper-button-next');
-                    let prevButton = document.querySelector('.swiper-button-prev');
-                    nextButton.classList.toggle('swiper-button-disabled', !swiper.isEnd);
-                    prevButton.classList.toggle('swiper-button-disabled', !swiper.isBeginning);
-                },
-                init: function () {
-                    
-                    let nextButton = document.querySelector('.swiper-button-next');
-                    let prevButton = document.querySelector('.swiper-button-prev');
-                    nextButton.classList.toggle('swiper-button-disabled', !swiper.isEnd);
-                    prevButton.classList.toggle('swiper-button-disabled', !swiper.isBeginning);
-                }
-            }
-        });
+import Swiper from 'swiper-bundle';
+
+new Scwiper('.scwiper-pr', {
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.projects-btn-next',
+    prevEl: '.projects-btn-prev',
+  },
+  slidesPerView: 1,
+  spaceBetween: 34,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
 
 
