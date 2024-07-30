76cbb393d15ca37c69e5e37bb735a6ff00f57611
ninaasieieva <160421199+ninaasieieva@users.noreply.github.com>
2024-07-30 21:07:30 +0300

    js acordion

--- src/js/aboutmesection.js
+++ src/js/aboutmesection.js
@@ -4,37 +4,37 @@ import 'swiper/css';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion', {
-duration: 600,
-showMultiple: true,
-openOnInit: [0],
+new Accordion('.about-me-accordion', {
+  duration: 600,
+  showMultiple: true,
+  openOnInit: [0],
 });
 
 new Swiper('.about-swiper', {
-slidesPerView: 2,
-spaceBetween: 1,
-speed: 1000,
-allowSlidePrev: false,
-loop: true,
-modules: [Navigation, Keyboard, Mousewheel],
-navigation: {
-nextEl: '.button-next',
-},
-keyboard: {
-enabled: false,
-onlyInViewport: false,
-},
-mousewheel: true,
-breakpoints: {
-768: {
-slidesPerView: 3,
-width: 600,
-loop: true,
-},
-1440: {
-slidesPerView: 6,
-width: 1200,
-loop: false,
-},
-},
+  slidesPerView: 2,
+  spaceBetween: 1,
+  speed: 1000,
+  allowSlidePrev: false,
+  loop: true,
+  modules: [Navigation, Keyboard, Mousewheel],
+  navigation: {
+    nextEl: '.button-next',
+  },
+  keyboard: {
+    enabled: false,
+    onlyInViewport: false,
+  },
+  mousewheel: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      width: 600,
+      loop: true,
+    },
+    1440: {
+      slidesPerView: 6,
+      width: 1200,
+      loop: false,
+    },
+  },
 });
