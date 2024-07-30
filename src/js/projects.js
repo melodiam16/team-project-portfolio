407ca6633cf8d0fbbd44e4722bae93759f9f739c
Michael Janzer <michaeljanzer1996@gmail.com>
2024-07-30 03:48:04 +0300

    CSS

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,33 +1,33 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-const btnNextEl = document.querySelector('.right-btn-next ');
-// btnNextEl.style.color = '#FAFAFA';
-// btnNextEl.style.position = 'static';
-// btnNextEl.style.width = '68px';
-// btnNextEl.style.height = '68px';
-// btnNextEl.style.margin = '0';
-const btnPrevEl = document.querySelector('.left-btn-prev ');
-// btnPrevEl.style.position = 'static';
-// btnPrevEl.style.width = '68px';
-// btnPrevEl.style.height = '68px';
-// btnPrevEl.style.color = '#FAFAFA';
-// btnPrevEl.style.margin = '0';
+// const btnNextEl = document.querySelector('.right-btn-next ');
+// // btnNextEl.style.color = '#FAFAFA';
+// // btnNextEl.style.position = 'static';
+// // btnNextEl.style.width = '68px';
+// // btnNextEl.style.height = '68px';
+// // btnNextEl.style.margin = '0';
+// const btnPrevEl = document.querySelector('.left-btn-prev ');
+// // btnPrevEl.style.position = 'static';
+// // btnPrevEl.style.width = '68px';
+// // btnPrevEl.style.height = '68px';
+// // btnPrevEl.style.color = '#FAFAFA';
+// // btnPrevEl.style.margin = '0';
 
-function upSwiperSlideDisplay() {
-  const swiperSlides = document.querySelectorAll('.swiper-slide-projects');
-  if (window.innerWidth > 1440) {
-    swiperSlides.forEach(slide => {
-      slide.style.display = 'flex';
-    });
-  } else {
-    swiperSlides.forEach(slide => {
-      slide.style.display = 'block';
-    });
-  }
-}
-upSwiperSlideDisplay();
-window.addEventListener('resize', upSwiperSlideDisplay);
+// function upSwiperSlideDisplay() {
+//   const swiperSlides = document.querySelectorAll('.swiper-slide');
+//   if (window.innerWidth > 1440) {
+//     swiperSlides.forEach(slide => {
+//       slide.style.display = 'flex';
+//     });
+//   } else {
+//     swiperSlides.forEach(slide => {
+//       slide.style.display = 'block';
+//     });
+//   }
+// }
+// upSwiperSlideDisplay();
+// window.addEventListener('resize', upSwiperSlideDisplay);
 
 export const projectsSwiper = new Swiper('.swiper', {
   slidesPerView: 3,
