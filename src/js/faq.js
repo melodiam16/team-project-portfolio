54b2a84e60599fd4ab75cf81244676815b014c76
Qvantt <65224747+Qvantt@users.noreply.github.com>
2024-07-30 08:48:09 +0300

    add

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,10 +1,18 @@
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   new Accordion('.accordion-container-second', {
+//     duration: 400,
+//     showMultiple: false,
+//   });
+// });
+// export { Accordion };
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  new Accordion('.accordion-container-second', {
-    duration: 400,
-    showMultiple: false,
-  });
+new Accordion('.accordion', {
+  duration: 600,
+  showMultiple: false,
 });
 export { Accordion };
