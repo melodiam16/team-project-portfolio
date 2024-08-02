a5660a8f142b9d1b36cc452ce6dd80344836cb44
melodiam16 <121240405+melodiam16@users.noreply.github.com>
2024-08-02 12:38:27 +0200

    change

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,29 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.querySelector('.hero-btn-mob');
-    const closeMenuBtn = document.querySelector('.header-close-btn');
-    const navMobile = document.querySelector('.mob-nav');
-    const navMenu = document.querySelector('.header-menu-wrap span');
+  const menuBtn = document.querySelector('.hero-btn-mob');
+  const closeMenuBtn = document.querySelector('.header-close-btn');
+  const navMobile = document.querySelector('.mob-nav');
+  const navMenu = document.querySelector('.header-menu-wrap span');
 
+  menuBtn.addEventListener('click', () => {
+    navMobile.classList.add('active');
+  });
 
-    menuBtn.addEventListener('click', () => {
-        navMobile.classList.add('active');
-    });
+  closeMenuBtn.addEventListener('click', () => {
+    navMobile.classList.remove('active');
+  });
 
-    closeMenuBtn.addEventListener('click', () => {
+  document
+    .querySelectorAll('.header-menu-link, .header-order-btn')
+    .forEach(anchor => {
+      anchor.addEventListener('click', function () {
         navMobile.classList.remove('active');
+      });
     });
 
-    document.querySelectorAll('.header-menu-link, .header-order-btn').forEach(anchor => {
-        anchor.addEventListener('click', function() {
-            navMobile.classList.remove('active');
-        });
-    });
+  navMenu.addEventListener('click', function () {
+    const menu = this.closest('.header-menu-wrap');
 
-    navMenu.addEventListener('click', function() {
-        const menu = this.closest('.header-menu-wrap');
-
-        if ( menu ) {
-            menu.classList.toggle('active');
-        }
-    });
+    if (menu) {
+      menu.classList.toggle('active');
+    }
+  });
 });
